713733acaf7fe55325e61cadb7624d1c71f069c7
Chris <Chris@uberchris.com>
2016-09-28 22:36:37 -0500

    add complete task option

--- WhoseTurn/Controllers/TasksController.cs
+++ WhoseTurn/Controllers/TasksController.cs
@@ -11,8 +11,18 @@ namespace WhoseTurn.Controllers
         {
             var myTasks = Db.Tasks.Where(t => t.TurnPersonId == id).ToList();
             var otherPeople = Db.People.Where(p => p.Id != id).Include(p => p.Tasks).ToList();
-            
+
             return View(new TasksView {MyTasks = myTasks, OtherPeople = otherPeople });
+        }
+
+        public ActionResult Complete(int id)
+        {
+            var task = Db.Tasks.Find(id);
+            var fromId = task.TurnPersonId;
+            var nextPerson = Db.People.First(p => p.Id != fromId);
+            task.TurnPerson = nextPerson;
+            Db.SaveChanges();
+            return RedirectToAction("Index", new { Id = fromId });
         }
     }
 }
