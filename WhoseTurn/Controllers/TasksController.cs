c0f83190dccf2712503688dd660e3b6b68119299
Chris <Chris@uberchris.com>
2016-09-28 21:37:57 -0500

    use db models directly

--- WhoseTurn/Controllers/TasksController.cs
+++ WhoseTurn/Controllers/TasksController.cs
@@ -1,12 +1,26 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace WhoseTurn.Controllers
 {
     public class TasksController : Controller
     {
+        private readonly DbModel _db = new DbModel();
+
         public ActionResult Index()
         {
-            return View();
+            var tasks = _db.Tasks.Include(t => t.TurnPerson);
+            return View(tasks.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
