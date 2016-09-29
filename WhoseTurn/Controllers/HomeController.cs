a61c9a2c130584af49f61f0a48eb14faaa25bd86
Chris <Chris@uberchris.com>
2016-09-28 21:57:20 -0500

    generate homepage from db

--- WhoseTurn/Controllers/HomeController.cs
+++ WhoseTurn/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
 
 namespace WhoseTurn.Controllers
 {
@@ -6,7 +7,8 @@ namespace WhoseTurn.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var people = Db.People;
+            return View(people.ToList());
         }
     }
 }
