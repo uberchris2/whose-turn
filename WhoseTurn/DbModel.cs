dfa6c7771388bff4106dd3b6fe25739a380acb9e
Chris <Chris@uberchris.com>
2016-09-28 21:03:21 -0500

    add db context

---
+++ WhoseTurn/DbModel.cs
@@ -0,0 +1,15 @@
+using WhoseTurn.Models;
+
+namespace WhoseTurn
+{
+    using System.Data.Entity;
+
+    public class DbModel : DbContext
+    {
+        public DbModel() : base("name=DbModel")
+        {
+        }
+         public virtual DbSet<Person> People { get; set; }
+         public virtual DbSet<Task> Tasks { get; set; }
+    }
+}
