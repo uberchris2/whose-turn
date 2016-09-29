c99e32067fbfee4a6c500211e5681feb2b6fb2dc
Chris <Chris@uberchris.com>
2016-09-28 19:52:11 -0500

    add darkly theme

--- WhoseTurn/App_Start/BundleConfig.cs
+++ WhoseTurn/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace WhoseTurn
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/bootstrap.darkly.css"));
         }
     }
 }
