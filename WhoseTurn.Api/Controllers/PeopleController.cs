2c01a2aca35ab50e1c14bb416aacb13c0e3aa41c
Chris <Chris@uberchris.com>
2016-10-10 20:20:04 -0500

    implement people controller

---
+++ WhoseTurn.Api/Controllers/PeopleController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WhoseTurn.Common.Models;
+using WhoseTurn.Common.Services;
+
+namespace WhoseTurn.Api.Controllers
+{
+    public class PeopleController : ApiController
+    {
+        private readonly PeopleService _peopleService = new PeopleService();
+
+        [ResponseType(typeof(IEnumerable<Person>))]
+        public IHttpActionResult GetTasks(string group)
+        {
+            var people = _peopleService.GetPeople(group);
+            return Ok(people);
+        }
+    }
+}
