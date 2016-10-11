0eb00307ea162102a0a7a694c4072376caf243ab
Chris <Chris@uberchris.com>
2016-10-10 20:16:44 -0500

    add api tasks controller

---
+++ WhoseTurn.Api/Controllers/TasksController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Http;
+using System.Web.Http.Description;
+using WhoseTurn.Common.Services;
+using WhoseTurn.Common.ViewModels;
+
+namespace WhoseTurn.Api.Controllers
+{
+    public class TasksController : ApiController
+    {
+        private readonly TaskService _taskService = new TaskService();
+
+        [ResponseType(typeof(TasksView))]
+        public IHttpActionResult GetTasks(int id)
+        {
+            return Ok(_taskService.GetTasks(id));
+        }
+
+        [Route("api/Tasks/Complete/{id}")]
+        public IHttpActionResult Complete(int id)
+        {
+            _taskService.CompleteTask(id);
+            return Ok();
+        }
+    }
+}
--- WhoseTurn.Api/Global.asax.cs
+++ WhoseTurn.Api/Global.asax.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
+﻿using System.Web.Http;
+using Newtonsoft.Json;
 
 namespace WhoseTurn.Api
 {
@@ -12,6 +8,7 @@ namespace WhoseTurn.Api
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.Re‌​ferenceLoopHandling = ReferenceLoopHandling.Ignore;
         }
     }
 }
